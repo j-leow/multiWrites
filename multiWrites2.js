e775102a9de0db4d339b9d3c131fa5454bdebcc7
Jules Leow <jleow@iupui.edu>
2017-09-04 21:22:02 -0700

    Add textContent method to replace string in the html.

---
+++ multiWrites2.js
@@ -0,0 +1,14 @@
+/*
+Filename: multiWrites2.js
+Written by: Jules Leow (JL)
+Purpose: The JavaScript code that controls the .textContent method that will affect the <div> with the 'textContentId'. .innerHTML will affect the <div> with the 'innerHTMLId'.
+Date: 09/04/2017
+Modification History:
+Original Build
+*/
+
+$(document).ready(function() {
+    var textContentMethodStr = 'I be spend\'n most o\' me time on th\' \'puter. \'S hard bein\' a nurd... \'Specially since I supposed t\' be a ruddy pirate!'; //Set a variable to the string I want.
+    var elOutput = document.getElementById('textContentId');
+    elOutput.textContent = textContentMethodStr;
+}); 
