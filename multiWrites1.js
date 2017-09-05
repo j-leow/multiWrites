81f056d5dfd90a89d73e149ef056d08451d58b05
Jules Leow <jleow@iupui.edu>
2017-09-04 21:01:33 -0700

    Add multiWrite1.js to multiWrite1.html.

---
+++ multiWrites1.js
@@ -0,0 +1,13 @@
+/*
+Filename: multiWrites1.js
+Written by: Jules Leow (JL)
+Purpose: The JavaScript code that controls the document.write() that will affect multiWrites1.html
+Date: 09/04/2017
+Modification History:
+Original Build
+*/
+
+$(document).ready(function() {
+    var writeMethodStr = 'Arrr! I be Cap\'n Jules Leow! Yer timbers be shiver\'n if ye knew the type-a pirate yer messin\' with!';
+    document.write(writeMethodStr);
+}); 
